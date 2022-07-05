5a3c44fafdec1390ccc227bd6315856907dcf458
Tom Salisbury <mrsaulzi@gmail.com>
2022-07-05 22:56:28 +0100

    Added Empty Tests
    Added empty unit tests for the specified cases

---
+++ Innovation.Tests/RepairOrderHandlerTests.cs
@@ -0,0 +1,35 @@
+namespace Innovation.Tests
+{
+    public class RepairOrderHandlerTests
+    {
+        [Fact]
+        public void Large_repair_orders_for_new_customers_should_be_closed()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Large_rush_hire_orders_should_always_be_closed()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void Large_repair_orders_always_require_authorisation()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void All_rush_orders_for_new_customers_always_require_authorisation()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void All_other_orders_should_be_confirmed()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
