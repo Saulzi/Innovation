1e1453f9cf2c901cec1f7bd65bc29f59a787f45f
Tom Salisbury <mrsaulzi@gmail.com>
2022-07-05 23:06:42 +0100

    Implemented Unit Tests
    Implemented unit tests for the following cases
    
    -Large repair orders for new customers should be closed
    -Large rush hire orders should always be closed
    -Large repair orders always require authorisation
    -All rush orders for new customers always require authorisation
    -All other orders should be confirmed
    
    note this does not take the order into account which is a requirement,

--- Innovation.Tests/RepairOrderHandlerTests.cs
+++ Innovation.Tests/RepairOrderHandlerTests.cs
@@ -2,34 +2,71 @@ namespace Innovation.Tests
 {
     public class RepairOrderHandlerTests
     {
+        RepairOrderHandler itemUnderTest = new RepairOrderHandler();
+
         [Fact]
         public void Large_repair_orders_for_new_customers_should_be_closed()
         {
-            throw new NotImplementedException();
+            var largeNewCustomerOrder = new RepairOrder()
+            {
+                IsLargeOrder = true,
+                IsNewCustomer = true
+            };
+
+            var result = itemUnderTest.HandleOrder(largeNewCustomerOrder);
+
+            result.Should().Be(OrderStatus.Closed);
         }
 
         [Fact]
         public void Large_rush_hire_orders_should_always_be_closed()
         {
-            throw new NotImplementedException();
+            var largeRushOrder = new RepairOrder()
+            {
+                IsLargeOrder = true,
+                IsRushOrder = true
+            };
+
+            var result = itemUnderTest.HandleOrder(largeRushOrder);
+
+            result.Should().Be(OrderStatus.Closed);
         }
 
         [Fact]
         public void Large_repair_orders_always_require_authorisation()
         {
-            throw new NotImplementedException();
+            var largeRepairOrder = new RepairOrder()
+            {
+                IsLargeOrder = true,
+                OrderType = OrderType.Repair
+            };
+
+            var result = itemUnderTest.HandleOrder(largeRepairOrder);
+
+            result.Should().Be(OrderStatus.AuthorisationRequired);
         }
 
         [Fact]
         public void All_rush_orders_for_new_customers_always_require_authorisation()
         {
-            throw new NotImplementedException();
+            var rushOrder = new RepairOrder()
+            {
+                IsRushOrder = true
+            };
+
+            var result = itemUnderTest.HandleOrder(rushOrder);
+
+            result.Should().Be(OrderStatus.AuthorisationRequired);
         }
 
         [Fact]
         public void All_other_orders_should_be_confirmed()
         {
-            throw new NotImplementedException();
+            var otherOrder = new RepairOrder();
+
+            var result = itemUnderTest.HandleOrder(otherOrder);
+
+            result.Should().Be(OrderStatus.Confirmed);
         }
     }
 }
---
+++ Innovation/RepairOrderHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Innovation
+{
+    public class RepairOrderHandler
+    {
+        public OrderStatus HandleOrder(RepairOrder order)
+        {
+            return OrderStatus.Confirmed;
+        }
+    }
+}
